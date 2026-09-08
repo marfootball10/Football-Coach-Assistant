104fa2ec0771a274083ffe5423b1e5fc930173d7
marfootball10 <markiaro13@gmail.com>
2026-09-08 19:32:28 +0200

    Adding adaptive formating

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ let training_btn = document.getElementById("training_btn")
 let dashboard_div = document.querySelector(".dashboard_content")
 let players_div = document.querySelector(".players_content")
 let training_div = document.querySelector(".training_content")
+let menu_toggle = document.querySelector(".menu_toggle")
+let left_sidebar = document.querySelector(".left_sidebar")
+let out_of_menu = document.querySelector(".out_of_menu")
+
+menu_toggle.addEventListener("click", function () {
+    left_sidebar.classList.add("active")
+    menu_toggle.style.display = 'none'
+    out_of_menu.style.display = 'flex'
+})
+
+out_of_menu.addEventListener('click', function () {
+    left_sidebar.classList.remove("active")
+    menu_toggle.style.display = 'block'
+    out_of_menu.style.display = 'none'
+})
 
 dashboard_btn.addEventListener("click", () => {
     players_div.style.display = "none"
