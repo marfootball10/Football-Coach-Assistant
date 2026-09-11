5cb3905630d2f5f9cd14b440b3d2ae53208046a9
marfootball10 <markiaro13@gmail.com>
2026-09-11 13:27:39 +0200

    Working with layout and Add player button

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let dashboard_btn = document.getElementById("dashboard_btn")
 let players_btn = document.getElementById("players_btn")
 let training_btn = document.getElementById("training_btn")
+let add_player_btn = document.querySelector('.add_player_btn')
 let dashboard_div = document.querySelector(".dashboard_content")
 let players_div = document.querySelector(".players_content")
 let training_div = document.querySelector(".training_content")
+let new_player_div = document.querySelector('.new_player')
 let menu_toggle = document.querySelector(".menu_toggle")
 let left_sidebar = document.querySelector(".left_sidebar")
 let out_of_menu = document.querySelector(".out_of_menu")
@@ -36,4 +38,11 @@ training_btn.addEventListener("click", () => {
     dashboard_div.style.display = "none"
     players_div.style.display = "none"
     training_div.style.display = "flex"
+})
+
+let players = []
+
+add_player_btn.addEventListener('click', function() {
+    players_div.style.display = 'none'
+    new_player_div.style.display = 'flex'
 })
